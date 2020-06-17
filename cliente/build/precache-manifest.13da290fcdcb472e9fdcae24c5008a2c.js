1d412f108798584fa746daa0c13729a1bb489a55
Dario Bnz <developfsa@gmail.com>
2020-06-17 10:31:51 -0300

    cliente

---
+++ cliente/build/precache-manifest.13da290fcdcb472e9fdcae24c5008a2c.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "9ba168027c9e0a9a4bf7",
+    "url": "/static/js/main.44fdd3ae.chunk.js"
+  },
+  {
+    "revision": "635dc697ee6a300954f2",
+    "url": "/static/js/2.fc8c4206.chunk.js"
+  },
+  {
+    "revision": "b528dd108fd5e3f08e911141a142099e",
+    "url": "/index.html"
+  }
+];
